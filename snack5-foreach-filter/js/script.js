87b6853e894a9845cdf5ea511e4d9941b278a3ed
Cristiano <cristiano.tarconi@gmail.com>
2021-01-20 22:36:31 +0100

    snack 5 - list filter

--- snack5-foreach-filter/js/script.js
+++ snack5-foreach-filter/js/script.js
@@ -11,3 +11,37 @@ const generaRandom = (min, max) => {
 
 // Creo array predefinito
 const carBrands = ['Lamborghini', 'Pagani', 'Aston Martin', 'Bugatti', 'Ferrari', 'Koenigsegg', 'Porsche', 'Bentley', 'Caterham', 'Radical', 'Ariel'];
+
+// Tramite due prompt inserisco il range (min e max) di posizioni da prelevare dall'array principale e verifico che questi due numeri non vadano in conflitto
+let min, max;
+let sentinel = true;
+do {
+    alert(`Inserisci due numeri da 0 a ${carBrands.length - 1}, rispettivamente per il numero minimo e il numero massimo del range da selezionare`);
+    min = parseInt(prompt('Inserisci il numero di partenza'));
+    max = parseInt(prompt('Inserisci il numero di arrivo'));
+
+    if (min < max && min >= 0 && max <= carBrands.length - 1) {
+        sentinel = false;
+    } else {
+        alert('I numeri inseriti non sono validi, riprova!');
+    }
+
+} while (sentinel);
+
+console.log(carBrands);
+console.log(min, max);
+
+const range = [];
+
+// Filtro elementi nell'array in base alla posizione e li pusho all'interno del nuovo array in base ai numeri di min e max ricavati in precedenza
+carBrands.filter((e, i) => {
+    if (i >= min && i <= max) {
+        range.push(e);
+        /* return console.log(`${i}: ${e}`); */
+    }
+    return range;
+});
+
+console.log(range);
+
+// Testato due versioni, la prima che prevede di stampare su console direttamente la successione di nomi della lista all'interno del range selezionato; la seconda che prevede di pushare gli elementi selezionati nel range all'interno di un altro array
